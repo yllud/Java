e0c62bc370c48e062f64adaac1238ae03f91e979
yllud <124111142+yllud@users.noreply.github.com>
2023-03-12 20:30:44 +0900

    Add files via upload

---
+++ listVO/src/uiConnect/main.java
@@ -0,0 +1,11 @@
+package uiConnect;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Signin name = new Signin();
+		name.signin();
+	}
+
+}
